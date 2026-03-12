a3dd4326761e971bfaad983d38bb1fecbe26ea44
Lynn <snoopy0pig@gmail.com>
2026-03-12 18:58:22 +0800

    chore: deprecated nature.js due to ToS restrictions
    Removing the crawler logic for nature.com. Automated web scraping of full-text content is not permitted by the publisher.

--- src/config/sources.js
+++ src/config/sources.js
@@ -1,5 +1,4 @@
 const scienceDailyStrategy = require('../strategies/scienceDaily');
-const natureStrategy = require('../strategies/nature');
 
 const SOURCES = [
     {
@@ -8,12 +7,7 @@ const SOURCES = [
         listUrl: 'https://www.sciencedaily.com/news/top/science/',
         enabled: false
     },
-    {
-        name: 'Nature News',
-        source: natureStrategy,
-        listUrl: 'https://www.nature.com/nature/articles?type=news',
-        enabled: true
-    }
+
 ];
 
 module.exports = { SOURCES, USER_AGENT: 'KnowledgeFeederBot/1.0' };
