f8334937fb9cb1656bf77e047eb9b0b0b9d0d882
Lynn <snoopy0pig@gmail.com>
2026-03-12 18:27:30 +0800

    feat: integrate Nature News strategy and disable ScienceDaily for testing - Replaced TechCrunch placeholder with natureStrategy. - Set Nature News to enabled: true. - Set ScienceDaily to enabled: false to facilitate isolated testing.

--- src/config/sources.js
+++ src/config/sources.js
@@ -1,24 +1,19 @@
 const scienceDailyStrategy = require('../strategies/scienceDaily');
-// const techCrunchStrategy = require('../strategies/techCrunch'); // unimplemented
+const natureStrategy = require('../strategies/nature');
 
-/**
- * List of all scraping tasks
- */
 const SOURCES = [
     {
         name: 'ScienceDaily Top News',
         source: scienceDailyStrategy,
         listUrl: 'https://www.sciencedaily.com/news/top/science/',
-        enabled: true
-    },
-    /* unimplemented
-    {
-        name: 'TechCrunch Startups',
-        source: techCrunchStrategy,
-        listUrl: 'https://techcrunch.com/category/startups/',
         enabled: false
+    },
+    {
+        name: 'Nature News',
+        source: natureStrategy,
+        listUrl: 'https://www.nature.com/nature/articles?type=news',
+        enabled: true
     }
-    */
 ];
 
 module.exports = { SOURCES, USER_AGENT: 'KnowledgeFeederBot/1.0' };
